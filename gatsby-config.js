32f8d6e317524bbb119056c29fe8b9743daa2608
Tory Dannenberg <rfdberg@gmail.com>
2021-02-25 20:16:46 -0700

    Delete boiler plate files and dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Tory's Site`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@tdberg21`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
